6eb638bc9c59c2e55372e04f879f74b890bd8c85
Anand Gaitonde <agaitonde@pivotal.io>
2020-08-05 10:51:19 -0400

    Try avoiding a line-break issue in Invoke-Sysprep
    that may be causing sysprep to not read the unattend.xml
    which would explain the Timezone-not-reset, and other missing-config
    issues
    
    [#174128738](https://www.pivotaltracker.com/story/show/174128738)
     Update the stemcell creation process to ensure UTC timezone
    
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>

--- modules/BOSH.Sysprep/BOSH.Sysprep.psm1
+++ modules/BOSH.Sysprep/BOSH.Sysprep.psm1
@@ -437,8 +437,7 @@ function Invoke-Sysprep()
       Create-Unattend -NewPassword $NewPassword -ProductKey $ProductKey `
         -Organization $Organization -Owner $Owner
 
-      Invoke-Expression -Command 'C:/windows/system32/sysprep/sysprep.exe /generalize /oobe `
-      /unattend:"C:/Windows/Panther/Unattend/unattend.xml" /quiet /quit'
+      Invoke-Expression -Command 'C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /unattend:"C:/Windows/Panther/Unattend/unattend.xml" /quiet /quit'
     }
     Default { Throw "Invalid IaaS '${IaaS}' supported platforms are: AWS, Azure, GCP and Vsphere" }
   }
